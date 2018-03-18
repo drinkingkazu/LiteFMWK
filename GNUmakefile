384025dcbc480adac3f0f14f239ec371a1504c85
drinkingkazu <kazuhiro@nevis.columbia.edu>
2018-03-18 08:38:37 -0700

    overhaul

---
+++ GNUmakefile
@@ -0,0 +1,24 @@
+
+ifndef MYSW_BASEDIR
+ERROR_MESSAGE := $(error MYSW_BASEDIR is not set... run configure.sh!)
+endif
+
+OSNAME          = $(shell uname -s)
+HOST            = $(shell uname -n)
+OSNAMEMODE      = $(OSNAME)
+
+include $(MYSW_BASEDIR)/Makefile/Makefile.${OSNAME}
+
+SUBDIRS := Kazu Example
+
+.phony: all clean
+
+all:
+	@echo "Start building..."
+	@for i in $(SUBDIRS); do ( echo "" && echo "Compiling $$i..." && $(MAKE) --directory=$$i) || exit $$?; done
+	@echo "Done!"
+clean:
+	@echo "Cleaning..."
+	@for i in $(SUBDIRS); do ( echo "" && echo "Cleaning $$i..." && $(MAKE) clean --directory=$$i) || exit $$?; done
+	@echo "Done!"
+
--- setup.sh
+++ setup.sh
@@ -1,88 +1,75 @@
 #!/bin/bash
 
 # clean up previously set env
-me="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
-export MYSW_DIR=$me;
-export MYSW_LIBDIR=$me/lib
+if [[ -z $FORCE_MYSW_BASEDIR ]]; then
+    where="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    export MYSW_BASEDIR=${where}
+else
+    export MYSW_BASEDIR=$FORCE_MYSW_BASEDIR
+fi
+export MYSW_BINDIR=$MYSW_BASEDIR/bin
+
+# set the build dir
+unset MYSW_BUILDDIR
+if [[ -z $MYSW_BUILDDIR ]]; then
+    export MYSW_BUILDDIR=$MYSW_BASEDIR/build
+fi
+export MYSW_LIBDIR=$MYSW_BUILDDIR/lib
+export MYSW_INCDIR=$MYSW_BUILDDIR/include
+mkdir -p $MYSW_BUILDDIR;
 mkdir -p $MYSW_LIBDIR;
+mkdir -p $MYSW_BINDIR;
+
 # Abort if ROOT not installed. Let's check rootcint for this.
 if [ `command -v rootcling` ]; then
     export MYSW_ROOT6=1
-else 
-    if [[ -z `command -v rootcint` ]]; then
-	echo
-	echo Looks like you do not have ROOT installed.
-	echo You cannot use LArLite w/o ROOT!
-	echo Aborting.
-	echo
-	return;
-    fi
-fi
-MYSW_OS=`uname -s`
-
-# Check compiler availability for clang++ and g++
-if [ -x "$GCC_FQ_DIR" ] ; then
-  # We're running a UPS distribution, likely build root with it. So let's use GCC
-  MYSW_CXX=g++
 else
-  # Try the OSX default install
-  MYSW_CXX=clang++
-fi
-
-if [ `command -v $MYSW_CXX` ]; then
-    export MYSW_CXX="$MYSW_CXX -std=c++11";
-else
-    MYSW_CXX=g++
-    if [[ -z `command -v $MYSW_CXX` ]]; then
-	echo
-	echo Looks like you do not have neither clang or g++!
-	echo You need one of those to compile LArLite... Abort config...
-	echo
-	return;
-    fi
-    export MYSW_CXX;
-    if [ $MYSW_OS = 'Darwin' ]; then
-	echo $MYSW_OS
-	echo
-	echo "***************** COMPILER WARNING *******************"
-	echo "*                                                    *"
-	echo "* You are using g++ on Darwin to compile LArLite.    *"
-	echo "* Currently LArLite assumes you do not have C++11    *"
-	echo "* in this combination. Contact the author if this is *"
-	echo "* not the case. At this rate you have no ability to  *"
-	echo "* compile packages using C++11 in LArLite.           *"
-	echo "*                                                    *"
-	echo "* Help to install clang? See manual/contact author!  *"
-	echo "*                                                    *"
-	echo "******************************************************"
-	echo 
-    fi
-fi
-if [[ -z $ROOTSYS ]]; then
     echo
-    echo "****************** PyROOT WARNING ********************"
-    echo "*                                                    *"
-    echo "* Did not find your \$ROOTSYS. To use PyROOT feature, *"
-    echo "* Make sure ROOT.py is installed (comes with ROOT).  *"
-    echo "* You need to export \$PYTHONPATH to include the dir  *"
-    echo "* where ROOT.py exists.                              *"
-    echo "*                                                    *"
-    echo "* Help to install PyROOT? See manual/contact author! *"
-    echo "*                                                    *"
-    echo "******************************************************"
+    echo Looks like you do not have ROOT6 installed.
+    echo You cannot use LArLite w/o ROOT6!
+    echo Aborting.
     echo
-else
-    export MYSW_CXXSTDFLAG=`python $MYSW_DIR/config/get_stdflag.py`
-    export PYTHONPATH=$ROOTSYS/lib:$PYTHONPATH;
+    return 1;
 fi
 
-export PATH=$MYSW_DIR/bin:$PATH
+# Check Numpy
+export MYSW_NUMPY=`$MYSW_BASEDIR/bin/check_numpy`
+
+# warning for missing support
+missing=""
+if [ $MYSW_NUMPY -eq 0 ]; then
+    missing+=" Numpy"
+else
+    MYSW_INCLUDES="${MYSW_INCLUDES} -I`python -c\"import numpy; print(numpy.get_include())\"`"
+fi
+if [[ $missing ]]; then
+    printf "\033[93mWarning\033[00m ... missing$missing support. Build without them.\n";
+fi
+
+echo
+printf "\033[93mLArCV\033[00m FYI shell env. may useful for external packages:\n"
+printf "    \033[95mMYSW_INCDIR\033[00m   = $MYSW_INCDIR\n"
+printf "    \033[95mMYSW_LIBDIR\033[00m   = $MYSW_LIBDIR\n"
+printf "    \033[95mMYSW_BUILDDIR\033[00m = $MYSW_BUILDDIR\n"
+
+export PATH=$MYSW_BINDIR:$PATH
 export LD_LIBRARY_PATH=$MYSW_LIBDIR:$LD_LIBRARY_PATH
-if [ $MYSW_OS = 'Darwin' ]; then
-    export DYLD_LIBRARY_PATH=$MYSW_LIBDIR:$DYLD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=$MYSW_LIBDIR:$DYLD_LIBRARY_PATH
+export PYTHONPATH=$MYSW_BASEDIR/python:$PYTHONPATH
+
+export MYSW_CXX=clang++
+if [ -z `command -v $MYSW_CXX` ]; then
+    export MYSW_CXX=g++
+    if [ -z `command -v $MYSW_CXX` ]; then
+        echo
+        echo Looks like you do not have neither clang or g++!
+        echo You need one of those to compile LArCaffe... Abort config...
+        echo
+        return 1;
+    fi
 fi
+
 echo
 echo "Finish configuration. To build, type:"
-echo "> cd \$MYSW_DIR"
-echo "> make"
+echo "> make "
 echo
